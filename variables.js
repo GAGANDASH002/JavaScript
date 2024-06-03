9df365b67e09e6d231e7a28b9e917f937b5a9e4c
GAGANDASH002 <gagandash002@gmail.com>
2024-06-03 21:18:53 +0530

    Variables in JS

---
+++ variables.js
@@ -0,0 +1,18 @@
+//Naming conventions
+
+//CamelCase
+let firstName = "Pete";//followed in JS
+//PascalCase
+let LastName= "Mitchell";
+//SnakeCase
+let middle_name = "Maverick";
+
+//using const keyword
+const price = 99;
+
+//taking input from user
+var num;
+num=prompt("enter ur favourite number")
+
+//Output to user
+alert("your number is" +num);
