944d65d9a0df9d70e18aabbbc2fc29e1d00508ec
GAGANDASH002 <gagandash002@gmail.com>
2024-06-05 21:27:32 +0530

    Important operators in JS

---
+++ operators.js
@@ -0,0 +1,19 @@
+var x=52 , y=10;
+x= x%y;//modulus 
+
+var a=10 , b=2;
+a=a**b;//exponentiation
+
+var x=7;
+x++;//increment operator
+
+x=-x;//unary operator
+
+var x="Namaste" , y="world"
+x= x + " " + y;//string concatenation 
+
+var x=3 == "3";//equal operator
+
+var x=3 === "3";//strict equal
+
+
