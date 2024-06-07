4aca0d729d74c7f130f417c144f228d07e712b3c
GAGANDASH002 <gagandash002@gmail.com>
2024-06-07 20:57:59 +0530

    useful string methods in JS

---
+++ methods.js
@@ -0,0 +1,12 @@
+var favMovie = "Top Gun";
+console.log(favMovie.length);//returns length of string object
+
+console.log(favMovie[0]);//returns char at index
+
+console.log(favMovie.indexOf("Gun"));//first occurence of substring
+
+console.log(favMovie.slice(2,7));//returns substring from specified index
+
+var pete = "hello I am MaveRick";
+console.log(pete.toUpperCase());//returns string as uppercase char
+console.log(pete.toLowerCase());//returns string as lowercase
