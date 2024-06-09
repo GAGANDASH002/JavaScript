b69581101bd81f11e160f05f57781f87ec332582
GAGANDASH002 <gagandash002@gmail.com>
2024-06-09 19:41:00 +0530

    reference datatypes in JS

---
+++ reference_datatypes.js
@@ -0,0 +1,19 @@
+//object creation
+let animal={
+    name : "Cow",
+    legs : 4,
+}
+
+console.log(animal);
+
+//dot notation
+console.log(animal.name);
+//bracket notation
+console.log(animal["name"]);
+
+
+//Arrays
+let selectBooks = ["Cant Hurt Me","Think and Grow rich"];
+console.log(selectBooks);
+
+console.log(selectBooks[0]);//array accessing
