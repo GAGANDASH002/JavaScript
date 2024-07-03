b8fefcdfab798ed285ac9d125c96fb1aa2691c51
GAGANDASH002 <gagandash002@gmail.com>
2024-07-03 19:39:15 +0530

    continous BG color changer using setInterval method

---
+++ projects/Color_Changer/UnlimitedColors/script.js
@@ -0,0 +1,33 @@
+// generate a random color
+
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#'
+    for ( let i = 0 ; i < 6; i++){
+        color+= hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+let intervalId;
+const StartChangingColor= function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBGColor,1000);
+    }
+
+    function changeBGColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const StopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+
+
+document.querySelector('#start').
+addEventListener('click',StartChangingColor)
+
+document.querySelector('#stop').
+addEventListener('click',StopChangingColor)
