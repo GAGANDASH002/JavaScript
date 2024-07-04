2412aee0fe8598c03a92f84448d652eaa89f974d
GAGANDASH002 <gagandash002@gmail.com>
2024-07-04 18:59:46 +0530

    initial commit

---
+++ oopJS/README.md
@@ -0,0 +1,25 @@
+# javascript and classes
+
+## OOP
+
+## Object
+- collection of properties and methods
+
+
+## why use OOP
+- OOP helps break down complex problems into smaller, more manageable pieces. Each class or object can represent a specific aspect of the application, making it easier to understand and maintain.
+
+## parts of OOP
+Object literal 
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ oopJS/oop.js
@@ -0,0 +1,41 @@
+// Object literal
+
+const user = {
+    username: "Gagan",
+    loginCount : 7,
+    signedIn : true,
+
+    getUserDetails(){
+        // console.log("Got user details from database")
+        // console.log(`Username: ${this.username}`);
+        console.log(this) // points to current context i.e current object
+    }
+}
+
+// console.log(user.username)
+// console.log(this); 
+console.log(user.getUserDetails())
+
+// *********Constructor Function***********
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.hello = function(){
+        console.log(`Hello , ${this.username}`)
+    }
+
+    return this
+}
+
+// if new keyword is not used then separate context are not created
+// and data gets overriden
+
+const userOne =  new User("hitesh", 12, true)
+const userTwo =  new User("ChaiAurCode", 11, false)
+console.log(userOne);
+console.log(userTwo);
+console.log(userOne.constructor)
+console.log(userOne instanceof User)
