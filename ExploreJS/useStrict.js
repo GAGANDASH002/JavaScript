d87c1b27d299087f02d523f9a3134008c5fad837
GAGANDASH002 <gagandash002@gmail.com>
2024-08-26 19:02:28 +0530

    use Strict mode in JavaScript

---
+++ ExploreJS/useStrict.js
@@ -0,0 +1,25 @@
+//function sum(num1, num2){
+//    return arguments[0] + arguments[1]
+//}
+
+//console.log(sum(10,20)) Output: 30
+
+//function sum(num1, num2){
+//    num1 = 100
+//    num2 = 200
+//    return arguments[0] + arguments[1]
+//}
+
+//console.log(sum(10,20)) Output: 300
+
+function sum(num1, num2){
+    'use strict'
+    num1 = 100
+    num2 = 200
+    return arguments[0] + arguments[1]
+}
+
+console.log(sum(10,20)) // Output: 30
+
+
+
